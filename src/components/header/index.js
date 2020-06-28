e7f9e172df48e9b868608541bf46042fd845c74f
sisidjidji <s.azibi@live.fr>
2020-06-28 14:56:14 -0500

    header componamt

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from '../auth/login'
+
+
+export default function Header (){
+    return(
+        <header>
+            <Login/>
+        </header>
+    )
+}
